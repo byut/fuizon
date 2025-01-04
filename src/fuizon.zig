efcc49c79c5a7d7e7a3f0aae1655a8d0c10b9767
Volodymyr But <vbut.dev+git@gmail.com>
2025-01-04 01:38:55 +0100

    feat: add basic event api

--- src/fuizon.zig
+++ src/fuizon.zig
@@ -1,3 +1,9 @@
+pub const event = @import("event.zig");
+pub const Event = event.Event;
+pub const KeyEvent = event.KeyEvent;
+pub const KeyEventType = event.KeyEventType;
+pub const ResizeEvent = event.ResizeEvent;
+
 test "root" {
     try @import("std").testing.expect(true);
 }
